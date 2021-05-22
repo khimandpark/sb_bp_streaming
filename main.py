83822b805625de71622ed1fdda08e718a39e425a
khimandpark <66541622+khimandpark@users.noreply.github.com>
2021-05-22 16:00:07 +0900

    up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-import tweepy
+# import tweepy
 import pygame
 import random
 import instaloader
@@ -9,7 +9,7 @@ from settings import *
 
 def makeRectText(x, y, message, size):
     FONT = pygame.font.Font(VIEW_FONT, size)
-    text_Title = FONT.render('{:,}'.format(int(message)), True, WHITE)
+    text_Title = FONT.render('{:,}'.format(int(message)), True, GRAY2)
     text_Rect = text_Title.get_rect()
     text_Rect.centerx = round(SCREEN_WIDTH / 4.4)
     text_Rect.x = x
@@ -26,7 +26,8 @@ def request_Youtube_API():
 pygame.init()
 pygame.display.set_caption(TITLE)
 
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+# screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((540, 960))
 clock = pygame.time.Clock()
 background_image = pygame.image.load(BACKGROUND)
 
@@ -35,16 +36,22 @@ prv_seconds = 0
 
 insta = instaloader.Instaloader()
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+# auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+# auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# api = tweepy.API(auth)
 
 bp_subs = request_Youtube_API()
-bp_prior_subs = str(int(bp_subs) + 69336)
+bp_prior_subs = str(int(bp_subs) + 9336)
 cnt = 1
+
+b_cnt = 0
 
 while True:
     seconds = (pygame.time.get_ticks()-start_ticks)/1000
+
+    # if int(seconds * 100) % 1000 == 0:
+    #     background_image = pygame.image.load(BACKGROUNDS[b_cnt % 4])
+    #     b_cnt += 1
 
     screen.blit(background_image, (0, 0))
 
@@ -63,23 +70,22 @@ while True:
         bp_subs = request_Youtube_API()
         cnt = 1
 
-        if int(bp_prior_subs) > int(bp_subs) + 100000:
-            bp_prior_subs = str(int(bp_prior_subs) - random.randint(20000, 40000))
+        if int(bp_prior_subs) > int(bp_subs) + 10000:
+            bp_prior_subs = str(int(bp_prior_subs) - random.randint(2000, 4000))
             print('blackpink down')
 
     # 트위터 데이터 가져오기 
-    if int(seconds) % 20 == 0:
-        try:
-            user = api.get_user(TWITTER_ACCOUNT)
-            twit_follower_cnt = user.followers_count
-            print(twit_follower_cnt)
-        except tweepy.TweepError as e:
-            print("Error code: {} with message: {}".format(e.api_code, e.message[0]['message']))
+    # if int(seconds) % 20 == 0:
+    #     try:
+    #         user = api.get_user(TWITTER_ACCOUNT)
+    #         twit_follower_cnt = user.followers_count
+    #         print(twit_follower_cnt)
+    #     except tweepy.TweepError as e:
+    #         print("Error code: {} with message: {}".format(e.api_code, e.message[0]['message']))
             
-
     makeRectText(POSITIONS[0][0], POSITIONS[0][1], insta_follower_cnt, FONT_SIZE)
     makeRectText(POSITIONS[1][0], POSITIONS[1][1], bp_prior_subs, FONT_SIZE)
-    makeRectText(POSITIONS[2][0], POSITIONS[2][1], twit_follower_cnt, FONT_SIZE)
+    # makeRectText(POSITIONS[2][0], POSITIONS[2][1], twit_follower_cnt, FONT_SIZE)
 
     if int(prv_seconds * 5) != int(seconds * 5):
         insta_follower_cnt += random.choice([0]*5 + list(range(0, 2)))
@@ -100,7 +106,6 @@ while True:
     pygame.display.update()
     clock.tick(10)
 
-    
     prv_seconds = seconds
 
 pygame.quit()
--- settings.py
+++ settings.py
@@ -1,21 +1,22 @@
 ########################### 수정이 필요한 세팅 값 ##########################
-TITLE = 'Blackpink Streaming'                          # 영상 타이틀 제목
-BACKGROUND = 'bp.png'                       # 영상 백그라운드 이미지
+TITLE = 'Lisa Streaming'                          # 영상 타이틀 제목
+BACKGROUND = 'lisa1.png'                       # 영상 백그라운드 이미지
+BACKGROUNDS = ['lisa1.png', 'lisa2.png', 'lisa3.png', 'lisa4.png']                      # 영상 백그라운드 이미지
 
 # 인스타
-INSTA_ID = 'blackpinkofficial'
+INSTA_ID = 'lalalalisa_m'
 
 # 유튜브
-YOUTUBE_ID = 'UCOmHUn--16B90oW2L6FRR3A'
+YOUTUBE_ID = 'UC35HKvKYPkri4Grd5KXl3wQ'
 YOUTUBE_KEY = 'AIzaSyBTuJW366a_cuwEZbPGm5-T7ZJiJtMkhu8'
 YOUTUBE_CHANNEL_URL = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={}&key={}'
 
 # 트위터
-CONSUMER_KEY = "St2VMQEAqSWV2SeIoiZb9h918"
-CONSUMER_SECRET = "xgUvRKY6yxEaRbI7qSDfBtKlj4KIgbrqloIUi4k6ejXjaAtScx"
-ACCESS_TOKEN = "1394273205490118660-bnuYqxNJN9mpwfcNvhxzjEqgXQzQ5u"
-ACCESS_TOKEN_SECRET = "UAPuVJ43GRMRMLaN1ziZmcq5g07PVJt74yuTegsdEwoR2"
-TWITTER_ACCOUNT = "ygofficialblink"
+# CONSUMER_KEY = "St2VMQEAqSWV2SeIoiZb9h918"
+# CONSUMER_SECRET = "xgUvRKY6yxEaRbI7qSDfBtKlj4KIgbrqloIUi4k6ejXjaAtScx"
+# ACCESS_TOKEN = "1394273205490118660-bnuYqxNJN9mpwfcNvhxzjEqgXQzQ5u"
+# ACCESS_TOKEN_SECRET = "UAPuVJ43GRMRMLaN1ziZmcq5g07PVJt74yuTegsdEwoR2"
+# TWITTER_ACCOUNT = "ygofficialblink"
 
 TIME_DELAY = 5                                      # API 호출 시간 단위(분)
 VIEW_FONT = './font/Roboto-Regular.ttf'          # 뷰 폰트
@@ -24,26 +25,27 @@ KEY = 'AIzaSyCqDwnddNCmxfARhDuzcwQylX11hHvycxo'     # 유튜브 API 키
 
 
 POSITIONS = [                                   # 각 영상 뷰 카운트 위치
-    (223, 926),    # 유튜브
-    (881, 926),    # 인스타
-    (1533, 926),    # 트위터
+    (200, 745),    # 인스타
+    (200, 875),    # 유튜브
+    (0, 0),    # 트위터
 ]
 
-FONT_SIZE = 53
+FONT_SIZE = 42
 
 ############################### 고정 세팅 값 ##################################
 SLACK_KEY = 'xoxb-1244329845713-1695066206707-wUEAy8aV3ejHD6IqoXHpcy6H'
 
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
+SCREEN_WIDTH = 540
+SCREEN_HEIGHT = 960
 
 # colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (100, 100, 100)
 LIGHT_GRAY = (148, 148, 148)
+GRAY2 = (131, 131, 131)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-YELLOW = (255 ,255 ,0)
+YELLOW = (255, 255, 0)
 BROWN = (111, 109, 81)
